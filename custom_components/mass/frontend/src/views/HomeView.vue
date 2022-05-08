753b3bc4f77c2cdf0dae2de32524532ee8047053
Marcel van der Veldt <m.vanderveldt@outlook.com>
2022-05-08 13:39:17 +0200

    add basic check for provider too

--- custom_components/mass/frontend/src/views/HomeView.vue
+++ custom_components/mass/frontend/src/views/HomeView.vue
@@ -28,7 +28,9 @@
           <v-divider />
           <span class="text-center text-subtitle-1" style="padding: 10px">
             {{ $t(card.key) }}
-          <span class="text-caption" v-if="api.stats[card.stats] > 0">({{ api.stats[card.stats] }})</span>
+            <span class="text-caption" v-if="api.stats[card.stats] > 0"
+              >({{ api.stats[card.stats] }})</span
+            >
           </span>
         </v-card>
       </v-col>
