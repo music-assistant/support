ac423e8580512ca5a079b4d645181dc04c05b472
Marcel van der Veldt <m.vanderveldt@outlook.com>
2024-06-13 16:07:24 +0200

    lint fixes

--- tests/test_config_flow.py
+++ tests/test_config_flow.py
@@ -16,10 +16,8 @@ from pytest_homeassistant_custom_component.common import MockConfigEntry
 from custom_components.mass import config_flow
 from custom_components.mass.config_flow import (
     CONF_ASSIST_AUTO_EXPOSE_PLAYERS,
-    CONF_INTEGRATION_CREATED_ADDON,
     CONF_OPENAI_AGENT_ID,
     CONF_URL,
-    CONF_USE_ADDON,
 )
 from custom_components.mass.const import DOMAIN
 
@@ -27,8 +25,6 @@ DEFAULT_TITLE = "Music Assistant"
 
 VALID_CONFIG = {
     CONF_URL: "http://localhost:8095",
-    CONF_USE_ADDON: False,
-    CONF_INTEGRATION_CREATED_ADDON: False,
     CONF_OPENAI_AGENT_ID: "2be183ad64ff0d464a94bd2915140a55",
     CONF_ASSIST_AUTO_EXPOSE_PLAYERS: True,
 }
@@ -115,33 +111,6 @@ async def test_flow_user_init_manual_schema(hass):
     data_schema = result.get("data_schema")
     assert data_schema is not None
     assert data_schema.schema[CONF_URL] is str
-    assert isinstance(
-        data_schema.schema[CONF_OPENAI_AGENT_ID], ConversationAgentSelector
-    )
-    assert data_schema.schema[CONF_ASSIST_AUTO_EXPOSE_PLAYERS] is bool
-
-
-async def test_flow_user_init_supervisor_schema(hass):
-    """Test the initialization of the form in the first step of the config flow."""
-    hass.config.components.add("hassio")
-    result = await hass.config_entries.flow.async_init(
-        config_flow.DOMAIN, context={"source": "user"}
-    )
-    expected = {
-        "data_schema": config_flow.ON_SUPERVISOR_SCHEMA,
-        "description_placeholders": None,
-        "errors": None,
-        "flow_id": mock.ANY,
-        "handler": "mass",
-        "step_id": "on_supervisor",
-        "last_step": None,
-        "preview": None,
-        "type": "form",
-    }
-    assert result.get("step_id") == expected.get("step_id")
-    data_schema = result.get("data_schema")
-    assert data_schema is not None
-    assert data_schema.schema[CONF_USE_ADDON] is bool
     assert isinstance(
         data_schema.schema[CONF_OPENAI_AGENT_ID], ConversationAgentSelector
     )
